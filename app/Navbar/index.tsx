93e1d5965cfdfb534ccf5ee2194267bdb7ec4c23
Evandro Viegas <99356220+EvandrooViegas@users.noreply.github.com>
2024-01-11 15:25:34 +0000

    aaa

--- app/Navbar/index.tsx
+++ app/Navbar/index.tsx
@@ -3,21 +3,18 @@ import React from "react";
 import Logo from "./Logo";
 import links from "./links";
 import Cart from "./Cart";
+
+export async function generateMetadata({ params }) {
+  return {
+    title: 'iWaveoutlet',
+  }
+}
+
 export default function Navbar() {
   return (
     <nav className="z-nav fixed top-0 inset-x-0  border-b border-neutral-800 bg-background">
       <div className="max-app-width mx-auto  px-12 py-2  flex items-center justify-between">
         <Logo />
-        <ul className="flex items-center  gap-5">
-          {links.map((link) => (
-            <li
-              key={link.name}
-              className="hover:underline text-neutral-300 text-sm "
-            >
-              <a href={link.link}>{link.name}</a>
-            </li>
-          ))}
-        </ul>
 
 <Cart />
 
